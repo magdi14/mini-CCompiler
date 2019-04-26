b3863c65ebe53144434423bf3f24df2df1aeec6b
Shehabeldeen <shehabeldeen555@gmail.com>
2019-04-27 00:39:51 +0200

    Args

---
+++ src/parser/Args.java
@@ -0,0 +1,15 @@
+package parser;
+
+public class Args implements Node {
+
+    Arg_list arg_list;
+
+    @Override
+    public void printNode() {
+        // TODO Auto-generated method stub
+        System.out.println("---------Args-----------");
+        System.out.println("arg_list = ");
+        if (arg_list != null) arg_list.printNode();
+        else System.out.println("null");
+    }
+}
