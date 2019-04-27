9c17fb066d060e225e996c7b183302b1a1b7bc65
mohamed magdy <mohamed14magdi@gmail.com>
2019-04-27 13:45:25 +0200

    Stmt_list2

---
+++ src/parser/Stmt_list2.java
@@ -0,0 +1,24 @@
+package parser;
+
+import Lexical.Token;
+
+public class Stmt_list2 implements Node{
+    Stmt stmt;
+    Stmt_list2 stmt_list2;
+
+    public Stmt_list2(Stmt stmt, Stmt_list2 stmt_list2) {
+        this.stmt = stmt;
+        this.stmt_list2 = stmt_list2;
+    }
+
+    @Override
+    public void printNode() {
+        if(stmt != null)
+        {
+            System.out.println(" Stmt = ");
+            stmt.printNode();
+            System.out.println(" Stmt_list2 = ");
+            stmt_list2.printNode();
+        }
+    }
+}
