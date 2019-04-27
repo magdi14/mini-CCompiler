560b749495b1c8411c34d696666de0b41e95e5c6
mohamed magdy <mohamed14magdi@gmail.com>
2019-04-27 13:30:20 +0200

    While-stmt

--- src/parser/While_stmt.java
+++ src/parser/While_stmt.java
@@ -1,4 +1,33 @@
 package parser;
 
-public class While_stmt {
+import Lexical.Token;
+
+public class While_stmt implements Node{
+    Token t1;
+    Token t2;
+    Token t3;
+    Expr expr;
+    Stmt stmt;
+
+    public While_stmt(Token t1, Token t2, Token t3, Expr expr, Stmt stmt) {
+        this.t1 = t1;
+        this.t2 = t2;
+        this.t3 = t3;
+        this.expr = expr;
+        this.stmt = stmt;
+    }
+
+    @Override
+    public void printNode() {
+        if(t1 != null)
+        {
+            System.out.println(t1.getValue());
+            System.out.println(t2.getValue());
+            System.out.println(" Expr = ");
+            expr.printNode();
+            System.out.println(t3.getValue());
+            System.out.println(" Stmt = ");
+            stmt.printNode();
+        }
+    }
 }
