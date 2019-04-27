03b0e69829a6fef7b33efa1c40bb293a6fcaf8c0
mohamed magdy <mohamed14magdi@gmail.com>
2019-04-27 13:42:47 +0200

    Expr_stmt

--- src/parser/Expr_stmt.java
+++ src/parser/Expr_stmt.java
@@ -1,4 +1,27 @@
 package parser;
 
-public class Expr_stmt {
+import Lexical.Token;
+
+public class Expr_stmt implements Node{
+    Token t1;
+    Expr expr;
+
+    public Expr_stmt(Token t1) {
+        this.t1 = t1;
+    }
+
+    public Expr_stmt(Token t1, Expr expr) {
+        this.t1 = t1;
+        this.expr = expr;
+    }
+
+    @Override
+    public void printNode() {
+        if(expr != null)
+        {
+            System.out.println(" Expr = ");
+            expr.printNode();
+        }
+        System.out.println(t1.getValue());
+    }
 }
